4902c522e559f1bf75631f1e30b7c565c57b841a
阳阳 <zhangsan@qq.com>
2016-11-24 10:01:59 +0800

    初始化地址

---
+++ books/books/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  books
+//
+//  Created by 阳阳 on 16/11/24.
+//  Copyright © 2016年 liuguyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
